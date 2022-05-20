6d834daf88bba51d321832c99367a082c97dd266
Mykael Lima <99366334+NinjaEvangelico@users.noreply.github.com>
2022-05-20 10:17:58 -0300

    Update categorias.js

--- routes/categorias.js
+++ routes/categorias.js
@@ -136,19 +136,6 @@ router.post('/:id', async (req, res) => {
 
 })
 
-//Filtro de categorias
-
-router.get('/filtro', (req, res) => {
-    Categoria.find().lean().then((categorias) => {
-        res.render('categorias/filtro', { categorias: categorias })
-    }).catch((err) => {
-        req.flash('error_msg', 'Houve um erro ao filtrar as categorias');
-        req.redirect('/categorias');
-    });
-
-});
-
-
 
 
 
